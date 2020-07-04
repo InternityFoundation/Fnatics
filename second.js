bac557928c69c471a992b7e8c1e9c20d291737ab
JyotirmayaV <jyotirmayavasaniwal@gmail.com>
2020-07-04 21:29:23 +0530

    morejs

--- second.js
+++ second.js
@@ -10,7 +10,7 @@ function create_json()
 		if(document.getElementById(skill).checked == true)
 		{
 			var level = document.getElementById("level"+i).value; //level2
-			var name = document.getElementById("name"+i).value;  //name2
+			var name = document.getElementById("name"+i).innerHTML;  //name2
 			json = { "name":name,"level":level};
 			//console.log(json);
 			skills[k++] = json;
@@ -18,10 +18,74 @@ function create_json()
 
 		}
 	}
-	var jsondata = {"skills":skills};
+
+
+	var workHistory = [];
+
+	var org_name = document.getElementById("org_name").value;
+	console.log(org_name);
+	var startDate = document.getElementById("startDate").value;
+	var endDate = document.getElementById("endDate").value;
+
+	startDate = startDate.split("-");
+	var start_month = startDate[1];
+	var start_year = startDate[0];
+
+	endDate = endDate.split("-");
+	var end_month = endDate[1];
+	var end_year = endDate[0];
+
+	var is_working = true;    //update this later
+	
+	var tenure = {"start_month":start_month,"start_year":start_year,"end_month":end_month,"end_year":end_year,"is_working":is_working};
+
+
+	var title = document.getElementById("title").value;
+
+	console.log(tenure);
+	console.log(title);
+
+	var prname = document.getElementById("prname").value;
+	var desc = document.getElementById("desc").value;
+	var cont = document.getElementById("cont").value;
+	var from = document.getElementById("from").value;
+	var to = document.getElementById("to").value;
+
+	var duration = {"from":from,"to":to};
+
+	var work_info = {"ProjectName":prname,"Description":desc,"Contribution":cont,"Duration":duration};
+
+	var works = [];
+	works[0] = work_info;
+
+	var organisation1 = {"orgName":org_name,"tenure":tenure,"title":title,"Work":works};
+	workHistory[0] = organisation1;
+
+
+	var jsondata = {"Skills":skills,"workHistory":workHistory};
 	var myJSON = JSON.stringify(jsondata);
-	//console.log(jsondata);
 	console.log(myJSON);
+
+
+	var 
+
 
 }
 
+
+
+
+
+
+[
+{
+"schoolName":"ABC School",
+"schoolBoard":"CBSE",
+"duration":{
+"from":"",
+"to":""
+},
+"standard":"10th",
+"result":"9.3cgpa"
+}
+]
